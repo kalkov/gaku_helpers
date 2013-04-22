cbecd0e51b313c8d019489a8f319de00700ad3dc
Vassil Kalkov <vassilkalkov@gmail.com>
2013-04-22 12:31:41 +0300

    Add modal_helper

--- lib/gaku_helpers/view_helpers.rb
+++ lib/gaku_helpers/view_helpers.rb
@@ -3,5 +3,6 @@ require 'gaku_helpers/view_helpers/link_helper'
 module GakuHelpers
   module ViewHelpers
     ActionView::Base.send :include, LinkHelper
+    ActionView::Base.send :include, ModalHelper
   end
 end
